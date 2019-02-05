5c4596e49a98374311f2ee11bb7e7c56e8f0a2a3
rafaelgm9 <rgarciamar@email.arizona.edu>
2019-02-05 12:43:25 -0700

    Substracted 1halo term from the matter-matter CF.

--- xihm_exclusion.py
+++ xihm_exclusion.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cluster_toolkit
+import fastcorr
 from colossus.halo.mass_so import M_to_R
+from scipy.special import sici
 from scipy.special import erfc
 from scipy.interpolate import interp1d
 from scipy.integrate import quad
@@ -11,16 +13,24 @@ rhocrit = 2.77536627e+11
 
 
 # Functions
-def xihm_model(r, rt, m, c, alpha, bias, ma, ca, mb, cb, ximm, Om):
+def xihm_model(r, rt, m, c, alpha, bias, ma, ca, mb, cb, xi2h, Om):
+    rhom = Om * rhocrit
+
     # Get xi1h
     xi1h = 1. + cluster_toolkit.xi.xi_nfw_at_R(r, 10**m, c, Om)
     xi1h *= thetat(r, rt, alpha)
 
+    # Substract 1halo term
+    r1 = M_to_R(m, z=0., mdef='200m')
+    rb = M_to_R(mb, z=0., mdef='200m')
+    rb = rt / r1 * rb
+    xi2h = xi2h - mb / rhom * utconvut(r, rb, cb)
+
     # Get xi2h
-    xi2h = bias * ximm
+    xi2h = bias * xi2h
 
     # Get correction
-    C = - bias * utconvthetae(r, rt, 10**m, alpha, 10**ma, ca, Om) * ximm - utconvthetae(r, rt, 10**m, alpha, 10**mb, cb, Om)
+    C = - bias * utconvthetae(r, rt, 10**m, alpha, 10**ma, ca, Om) * xi2h - utconvthetae(r, rt, 10**m, alpha, 10**mb, cb, Om)
 
     # Full xi
     xi = xi1h + xi2h + C
@@ -80,5 +90,25 @@ def utconvthetae(r, rt1, mass1, alpha, mass2, conc2, Om):
             i += 1
         integral[j] = 2 * np.pi * simps(g_at_u, u, even='first')
         j += 1
+
+    return integral
+
+
+def utconvut(r, rt, conc):
+    # Get ut(k)
+    # Analytic expression
+    k = np.logspace(-4, 4, num=1000, base=10)
+    rc = rt / conc
+    mu = k * rc
+    Put = (np.cos(mu) * (sici(mu + mu * conc)[1] - sici(mu)[1])
+           + np.sin(mu) * (sici(mu + mu * conc)[0] - sici(mu)[0])
+           - np.sin(mu * conc) / (mu + mu * conc)) \
+          / (np.log(1 + conc) - conc / (1 + conc))
+
+    # Product
+    PutPut = Put * Put
+
+    # Transform to real space
+    integral = fastcorr.calc_corr(r, k, PutPut, N=500, h=1e-2)
 
     return integral
